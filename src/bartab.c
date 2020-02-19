875c012c81212fc2e6ca0bfbe98945906abf07eb
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2020-02-19 16:55:45 +0100

    Add API to select channels in a tab
    Add new public function: mcp_select_tab_channels() which takes a list of
    channel indexes to select. All the other channels will be disabled.
    
    Closes: #2
    Change-Id: Ib99cdfa00908116a46c0be6d231efe0d301d4469

--- src/bartab.c
+++ src/bartab.c
@@ -399,6 +399,17 @@ void bartab_define_input(struct signaltab* tab, const char** labels)
 	init_filter(brtab);
 }
 
+static
+void bartab_select_channels(struct signaltab* tab, int nch,
+                            int const * indices)
+{
+	struct bartab* brtab = get_bartab(tab);
+	GtkTreeView* treeview = GTK_TREE_VIEW(brtab->widgets[ELEC_TREEVIEW]);
+	GtkTreeSelection* selection = gtk_tree_view_get_selection(treeview);
+
+	select_channels(selection, nch, indices);
+}
+
 
 static
 void bartab_process_data(struct signaltab* tab, unsigned int ns,
@@ -480,6 +491,7 @@ struct signaltab* create_tab_bargraph(const struct tabconf* conf)
 	// Initialilize the parent class
 	brtab->tab.destroy = bartab_destroy;
 	brtab->tab.define_input = bartab_define_input;
+	brtab->tab.select_channels = bartab_select_channels;
 	brtab->tab.process_data = bartab_process_data;
 	brtab->tab.update_plot = bartab_update_plot;
 	brtab->tab.set_wndlen = NULL;
--- src/scopetab.c
+++ src/scopetab.c
@@ -739,6 +739,17 @@ void scopetab_define_input(struct signaltab* tab, const char** labels)
 
 
 static
+void scopetab_select_channels(struct signaltab* tab, int nch,
+                              int const * indices)
+{
+	struct scopetab* sctab = get_scopetab(tab);
+	GtkTreeView* treeview = GTK_TREE_VIEW(sctab->widgets[ELEC_TREEVIEW]);
+	GtkTreeSelection* selection = gtk_tree_view_get_selection(treeview);
+
+	select_channels(selection, nch, indices);
+}
+
+static
 void scopetab_process_data(struct signaltab* tab, unsigned int ns,
                            const float* in)
 {
@@ -825,6 +836,7 @@ struct signaltab* create_tab_scope(const struct tabconf* conf)
 	
 	sctab->tab.destroy = scopetab_destroy;
 	sctab->tab.define_input = scopetab_define_input;
+	sctab->tab.select_channels = scopetab_select_channels;
 	sctab->tab.process_data = scopetab_process_data;
 	sctab->tab.process_events = scopetab_process_events;
 	sctab->tab.update_plot = scopetab_update_plot;
